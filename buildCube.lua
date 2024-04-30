3b6ef9fff246101a424708f064b4aeca651f360a
Brandon Bounsophinh <36938689+Beninix@users.noreply.github.com>
2024-04-30 14:19:59 -0400

    Update buildCube.lua

--- buildCube.lua
+++ buildCube.lua
@@ -1,8 +1,8 @@
 args = {...}
-len = args[1]
+len = tonumber(args[1])
 print("Building a " .. len .. " by " .. len .. " by " .. len  .. " cube.")
 slot = turtle.getSelectedSlot() 
-function action()
+function place()
     while(turtle.getItemCount(slot)<1) do
       if(slot<16)
       then
@@ -12,59 +12,53 @@ function action()
       end
     turtle.select(slot)
     end
-    turtle.placeDown()
-    turtle.forward()
+	turtle.placeDown()
+end
+
+function nline(n)
+	for i=1,n or 1,1 do
+		place()
+		if i~=n then
+			turtle.forward()
+		end
+	end
 end
 
 function buildPlane()
-    for m=1,len,1
-    do
-        for n=1,len,1
-        do
-            action()
-        end
-        if(m==tonumber(len))
-        then
-            break
-        end
-        if(m%2==1)
-        then
-            turtle.turnRight()
-            turtle.forward()
-            turtle.turnRight()
-            turtle.forward()
-        else
-            turtle.turnLeft()
-            turtle.forward()
-            turtle.turnLeft()   
-            turtle.forward()
-        end
-    end
+	for m=1,len do
+		nline(len)
+		if(m==len)then
+		    break
+		end
+		if(m%2==1)
+		then
+		    turtle.turnRight()
+		    turtle.forward()
+		    turtle.turnRight()
+		else
+		    turtle.turnLeft()
+		    turtle.forward()
+		    turtle.turnLeft()   
+		end
+	end
+	turtle.up()
+	turtle.turnRight()
+	if len%2==1 then
+		turtle.turnRight()
+	end
 end
-buildPlane()
 
-if(len%2==1)
-then
-    turtle.turnRight()
-    turtle.turnRight()
-else
-	turtle.back()
-    turtle.turnRight()
-	turtle.back()
+function buildWall()
+	for i=1,len-2 do
+		for i=1,4 do
+			nline(len)
+			turtle.turnRight()
+		end
+		turtle.up()
+	end
 end
-turtle.forward()
-for l=1,len-2,1
-do
-turtle.up()
-  for m=1,4,1
-  do
-      for n=1,len-1,1
-      do
-          action()
-      end
-      turtle.turnRight()
-  end
-end
-turtle.up()
+
+buildPlane()
+buildWall()
 buildPlane()
 print("Done.")
