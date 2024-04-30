7cacb09f9b825bcb9776bad166d3cd9ba614d7d0
Brandon Bounsophinh <36938689+Beninix@users.noreply.github.com>
2024-04-30 11:11:18 -0400

    Update buildCube.lua
    Redoing the program

--- buildCube.lua
+++ buildCube.lua
@@ -2,7 +2,7 @@ args = {...}
 len = args[1]
 print("Building a " .. len .. " by " .. len .. " by " .. len  .. " cube.")
 slot = turtle.getSelectedSlot() 
-function action()
+function place()
     while(turtle.getItemCount(slot)<1) do
       if(slot<16)
       then
@@ -13,58 +13,40 @@ function action()
     turtle.select(slot)
     end
     turtle.placeDown()
-    turtle.forward()
 end
 
-function buildPlane()
-    for m=1,len,1
-    do
-        for n=1,len,1
-        do
-            action()
-        end
-        if(m==tonumber(len))
-        then
-            break
-        end
-        if(m%2==1)
-        then
-            turtle.turnRight()
-            turtle.forward()
-            turtle.turnRight()
-            turtle.forward()
-        else
-            turtle.turnLeft()
-            turtle.forward()
-            turtle.turnLeft()   
-            turtle.forward()
-        end
+function nline(n)
+    for i=1,n do
+	action()
+	if i!=n do
+		turtle.forward()
+	end
     end
 end
-buildPlane()
 
-if(len%2==1)
-then
-    turtle.turnRight()
-    turtle.turnRight()
-else
-	turtle.back()
-    turtle.turnRight()
-	turtle.back()
+--Build Plane
+nline(x)
+for i=x-1,1,-1 do
+    nline(i)
+    nline(i)
 end
-turtle.forward()
-for l=1,len-2,1
-do
-turtle.up()
-  for m=1,4,1
-  do
-      for n=1,len-1,1
-      do
-          action()
-      end
-      turtle.turnRight()
-  end
+--Build Walls
+place()
+	for j=1,x-2 do
+		for i=1,3 do
+			nline(x-1)
+			nline(x-1)
+			nline(x-1)
+		end
+		turtle.forward()
+		turtle.turnRight()
+		turtle.up()
+		place()
+	end
+--Build Plane
+nline(x)
+for i=x-1,1,-1 do
+    nline(i)
+    nline(i)
 end
-turtle.up()
-buildPlane()
 print("Done.")
